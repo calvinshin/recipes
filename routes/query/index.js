10b39c4ad502cf4d137d0665669348127608562e
Calvin Shin <calvin.shin@gmail.com>
2019-08-28 00:24:46 -0700

    Made edits to the working cheerio file

--- routes/query/index.js
+++ routes/query/index.js
@@ -1,12 +1,47 @@
 const router = require("express").Router();
 const axios = require("axios")
+const cheerio = require("cheerio")
 
 router.get("/allrecipes", (req, res) => {
     // Receiving the req.query.url as the value of the recipe
     axios.get(req.query.url)
         .then(response => {
             console.log("Complete extraction!")
-            res.send(response.data)
+
+            // Use cheerio to load the data and set an object recipe
+            $ = cheerio.load(response.data)
+            let recipe = {};
+            recipe.ingredients = [];
+            recipe.steps = [];
+            recipe.source = req.query.url;
+
+            // Set the title
+            recipe.title = $("#recipe-main-content").text()
+            
+            // Set the ingredients
+            $(".recipe-ingred_txt").each(function(i, element) {
+                let ingredient = $(this).text();
+                if(ingredient === "" || ingredient === "Add all ingredients to list") {
+                    // do nothing
+                }
+                else {
+                    recipe.ingredients.push(ingredient)
+                };
+            })
+
+            // Set the steps
+            $(".recipe-directions__list--item").each(function(i, element) {
+                let step = $(this).text().trim();
+                if(step === "") {
+                    // do nothing
+                }
+                else {
+                    recipe.steps.push(step);
+                };
+            })
+
+            // Send the recipe after being parsed
+            res.send(recipe)
         });
 
 
