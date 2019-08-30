c4739b485c47ba108c9c2464d6e2b8e0f178c424
Calvin Shin <calvin.shin@gmail.com>
2019-08-29 21:26:23 -0700

    Started form for ingredients that will be pushed into mongo from front-end

--- client/src/pages/home/index.js
+++ client/src/pages/home/index.js
@@ -3,7 +3,30 @@ import React, { Component } from "react";
 class Home extends Component {
     render() {
         return(
-            "Hello"
+            <form>
+                Title
+                <br />
+                <input type="text" name="title" />
+
+                <p />
+
+                Ingredients
+                <br />
+                <input type="textarea" name="ingredients" />
+
+                <p />
+
+                Steps
+                <br />
+                <input type="textarea" name="steps" />
+
+                <p />
+
+                Source
+                <br />
+                <input type="text" name="source" />
+
+            </form>
         )
     }
 }
